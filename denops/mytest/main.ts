28e2ea6c1721d7ef30f5623975848eee0d26795e
mochi-MizLab <mochice.mls.ntl@gmail.com>
2021-07-26 11:45:13 +0900

    make it work on vim and newvim

--- denops/mytest/main.ts
+++ denops/mytest/main.ts
@@ -7,6 +7,55 @@ import {
 } from "https://deno.land/x/unknownutil@v1.0.0/mod.ts";
 import * as popup from "https://deno.land/x/denops_popup@v2.0.1/mod.ts";
 
+// TODO
+// How to setting without move their buffer ?
+async function baddWithNofile(denops: Denops, name: string): Promise<void> {
+  await execute(denops, `badd ${name}`);
+  const bufnr = await denops.call("bufnr", `^${name}$`) as number;
+  const currentBufnr = await denops.call("bufnr", "%") as number;
+  await execute(denops, `buffer ${bufnr}`);
+  await execute(
+    denops,
+    `
+    setlocal bufhidden=hide buftype=nofile
+    setlocal noswapfile nobuflisted
+    setlocal nomodified
+    setlocal fileformat=unix
+    `,
+  );
+  await execute(denops, `buffer ${currentBufnr}`);
+}
+
+async function findBufFromName(denops: Denops, name: string): Promise<number> {
+  const bufExists = await denops.call("bufexists", name) as boolean;
+  let bufnr: unknown;
+  if (bufExists) {
+    bufnr = await denops.call("bufnr", `^${name}$`);
+    ensureNumber(bufnr);
+    return bufnr;
+  } else {
+    await baddWithNofile(denops, name);
+    bufnr = await findBufFromName(denops, name);
+    ensureNumber(bufnr);
+    return bufnr;
+  }
+}
+
+async function makeEmptyBuffer(denops: Denops): Promise<number> {
+  if (await denops.eval("has('nvim')") as number == 1) {
+    const bufnr = await denops.call("nvim_create_buf", false, true);
+    ensureNumber(bufnr);
+    return bufnr;
+    // Promise.resolve(bufnr);
+  } else {
+    // execute(denops, `badd popup`);
+    await baddWithNofile(denops, "popup");
+    return findBufFromName(denops, "popup");
+    // how to get it in vim ?
+    // return = -1;
+  }
+}
+
 export async function main(denops: Denops): Promise<void> {
   denops.dispatcher = {
     async dpsTest(text: unknown): Promise<unknown> {
@@ -22,15 +71,11 @@ export async function main(denops: Denops): Promise<void> {
         border: true,
       };
 
-      let bufnr: unknown; //empty buffer
-      if (await denops.eval("has('nvim')") as number == 1) {
-        bufnr = await denops.call("nvim_create_buf", false, true);
-      } else {
-        // how to get it in vim ?
-        bufnr = -1;
-      }
-      ensureNumber(bufnr);
+      // let bufnr: unknown; //empty buffer
+      const bufnr = await makeEmptyBuffer(denops);
+
       console.log(bufnr);
+      ensureNumber(bufnr);
 
       await execute(
         denops,
