1d75747663828d125d7c12c9b7fb118061315ef8
mochi-MizLab <mochice.mls.ntl@gmail.com>
2021-08-02 15:21:06 +0900

    :hammer: resolve #3

--- denops/popup-test/deps.ts
+++ denops/popup-test/deps.ts
@@ -1,9 +1,5 @@
 export type { Denops } from "https://deno.land/x/denops_std@v1.0.0/mod.ts";
 export { execute } from "https://deno.land/x/denops_std@v1.0.0/helper/mod.ts";
 export * as autocmd from "https://deno.land/x/denops_std@v1.0.0/autocmd/mod.ts";
-export {
-  ensureArray,
-  ensureNumber,
-  isNumber,
-} from "https://deno.land/x/unknownutil@v1.0.0/mod.ts";
-export * as popup from "https://deno.land/x/denops_popup@v2.0.1/mod.ts";
+export { ensureNumber } from "https://deno.land/x/unknownutil@v1.0.0/mod.ts";
+export * as popup from "https://raw.githubusercontent.com/Omochice/denops-popup/change-relative/mod.ts";
