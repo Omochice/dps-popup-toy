a2c9440b6edba88416f720582557b531fd9df2db
GitHub <noreply@github.com>
2022-03-25 00:59:46 +0000

    :package: update deno dependencies

--- denops/popup-toy/deps.ts
+++ denops/popup-toy/deps.ts
@@ -1,10 +1,10 @@
-export type { Denops } from "https://deno.land/x/denops_std@v3.0.0/mod.ts";
-export * as vars from "https://deno.land/x/denops_std@v3.0.0/variable/mod.ts";
-export { execute } from "https://deno.land/x/denops_std@v3.0.0/helper/mod.ts";
-export * as autocmd from "https://deno.land/x/denops_std@v3.0.0/autocmd/mod.ts";
+export type { Denops } from "https://deno.land/x/denops_std@v3.2.0/mod.ts";
+export * as vars from "https://deno.land/x/denops_std@v3.2.0/variable/mod.ts";
+export { execute } from "https://deno.land/x/denops_std@v3.2.0/helper/mod.ts";
+export * as autocmd from "https://deno.land/x/denops_std@v3.2.0/autocmd/mod.ts";
 export {
   ensureArray,
   ensureNumber,
   isString,
-} from "https://deno.land/x/unknownutil@v1.1.4/mod.ts";
+} from "https://deno.land/x/unknownutil@v2.0.0/mod.ts";
 export * as popup from "https://pax.deno.dev/Omochice/denops-popup@dev/mod.ts";
